417a6ae540c36779fee7c3980cb0c390d2a11515
lilianabarcia <lilianabarciap@gmail.com>
2022-10-04 23:00:14 +0100

    Scrimba React info-site creation

---
+++ info-site/src/index.js
@@ -0,0 +1,13 @@
+const navbar = (
+  <nav>
+      <h1>Bob's Bistro</h1>
+      <ul>
+          <li>Menu</li>
+          <li>About</li>
+          <li>Contact</li>
+      </ul>
+  </nav>
+)
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(navbar, document.getElementById("root"))
