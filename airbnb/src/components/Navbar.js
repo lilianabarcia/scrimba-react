26dfa254a4479e4ba367d776d40b10f312da28ea
Liliana Barcia <72565804+lilianabarcia@users.noreply.github.com>
2022-10-11 21:40:01 +0100

    Delete airbnb directory

--- airbnb/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Navbar() {
-    return (
-        <nav>
-            <img src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-Logo.png" alt="logo" />
-        </nav>
-    )
-}
