299a310aa67fa790e5d51b7a8c65e4e480b2e551
lilianabarcia <lilianabarciap@gmail.com>
2022-10-11 19:20:50 +0100

    airbnb navbar and first section creation

---
+++ airbnb/src/components/Navbar.js
@@ -0,0 +1,9 @@
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-Logo.png" alt="logo" />
+        </nav>
+    )
+}
