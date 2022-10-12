767b80cd193d79e488a7a755bdc0fdb0c4b414d2
lilianabarcia <lilianabarciap@gmail.com>
2022-10-13 00:00:53 +0100

    info-site creation

--- info-site/src/index.js
+++ info-site/src/index.js
@@ -1,13 +1,14 @@
-const navbar = (
-  <nav>
-      <h1>Bob's Bistro</h1>
-      <ul>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-      </ul>
-  </nav>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-// eslint-disable-next-line no-undef
-ReactDOM.render(navbar, document.getElementById("root"))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
